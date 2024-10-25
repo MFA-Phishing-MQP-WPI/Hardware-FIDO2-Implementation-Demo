4b758a5e04e0dd2c1d83ec6fef521f2be51754f9
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-10-25 18:28:56 -0400

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import sysconfig
 
 running_shell = None
 mycheck='/usr/bin/md5sum'
@@ -13,3 +14,10 @@ def running_on_PowerShell() -> bool:
     return running_on_shell() == 'PowerShell'
 def is_running_on_mac() -> bool:
     return platform.system() == 'Darwin'
+def is_externally_managed() -> bool:
+    try:
+        extern_managed = sysconfig.get_config_var('PYTHONFRAMEWORK') is None
+        return True
+    except Exception as e:
+        print(f"Error detecting system management: {e}")
+        return False
